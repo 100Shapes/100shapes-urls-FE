5f24f922d40c927f315bda89e86923212b322550
Michele Memoli <michele@100shapes.com>
2015-08-29 13:30:19 +0100

    Refactored default layout

--- src/states/main/index.js
+++ src/states/main/index.js
@@ -1,23 +1,14 @@
 export default ngModule => {
 
     ngModule
-        .config(function($stateProvider) {
-
-            $stateProvider
-                .state('main', {
-                    url: '',
-                    template: require('./main.html'),
-                    controller: 'MainCtrl as vm'
-                });
-        });
-
-    ////////////////
-
-    ngModule
         .controller('MainCtrl', MainCtrl);
 
-    function MainCtrl() {
+    function MainCtrl($mdSidenav) {
+        let vm = this;
 
+        vm.toggleSidenav = (menuId) => {
+            $mdSidenav(menuId).toggle();
+        };
     }
 
 };
